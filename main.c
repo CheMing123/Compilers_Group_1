c28da8528a583f3e8f3422baf668fc1e74341ced
VictorDanielBlandon <69736950+VictorDanielBlandon@users.noreply.github.com>
2025-04-21 12:08:08 +0800

    Updated the main
    Lexer functionality updated, works perfectly

--- main.c
+++ main.c
@@ -4,34 +4,34 @@
 #include <string.h>
 
 typedef enum{
-    OPEN_PARENTHESES,
-    CLOSE_PARENTHESES,
-    SEMI_COLUMN,
-} TypeSeparator;
-
-typedef enum{
-    EXIT,
-} TypeKeyword;
-
-typedef enum{
     INT,
-} TypeLiteral;
+    KEYWORD,
+    SEPARATOR,
+} TokenType;
 
 typedef struct{
-    TypeKeyword type;
-} TokenKeyword;
-    
-typedef struct{
-    TypeLiteral type;
-    char *value; // knew this had to change to char for it to pass anytime soon
-} TokenLiteral;
+    TokenType type;
+    char *value;
+} Token;
 
-typedef struct{
-    TypeSeparator type;
-} TokenSeparator;
+void print_token(Token token){
+    for(int i = 0; token.value[i] != '\0'; i++){
+        printf("%c", token.value[i]);
+    }
+    printf("\n");
+    if(token.type == INT){
+        printf("TOKEN TYPE: INT\n");
+    }    
+    else if(token.type == KEYWORD){
+        printf("TOKEN TYPE: KEYWORD\n");
+    }
+    else if(token.type == SEPARATOR){
+        printf("TOKEN TYPE: SEPARATOR\n");
+    }
+}
 
-TokenLiteral *generate_number(char *current, int *current_index){
-    TokenLiteral *token = malloc(sizeof(TokenLiteral));
+Token *generate_number(char *current, int *current_index){
+    Token *token = malloc(sizeof(Token));
     token->type = INT;
     char *value = malloc(sizeof(char) * 8);
     int value_index = 0;
@@ -41,26 +41,27 @@ TokenLiteral *generate_number(char *current, int *current_index){
         }
         value[value_index] = current[*current_index];
         value_index++;
-        current_index++;
+        *current_index += 1;
     }
+    value[value_index] = '\0';
     token->value = value;
     return token;
 }
 
-TokenKeyword *generate_keyword(char *current, int *current_index){
-    TokenKeyword *token = malloc(sizeof(TokenKeyword));
+Token *generate_keyword(char *current, int *current_index){
+    Token *token = malloc(sizeof(Token));
     char *keyword = malloc(sizeof(char) * 8);
     int keyword_index = 0;
     while(isalpha(current[*current_index]) && current[*current_index] != '\0'){
         keyword[keyword_index] = current[*current_index];
-        printf("keyword: %c", current[*current_index]);
         keyword_index++;
-        current_index++;
+        *current_index += 1;
     }
-    printf("\n");
+    keyword[keyword_index] = '\0';
     if(strcmp(keyword, "exit") == 0){
         printf("TYPE EXIT\n");
-        token->type = EXIT;
+        token->type = KEYWORD;
+        token->value = "EXIT";
     }
     return token;
 }
@@ -89,13 +90,15 @@ void lexer(FILE *file){
         else if(current[current_index] == ')'){
             printf("FOUND CLOSE PARENTHESES\n");
         }
-        //finding a way to determine nums and strings
         else if(isdigit(current[current_index])){
-            TokenLiteral *test_token = generate_number(current, &current_index);
-            printf("TEST TOKEN VALUE = %s\n", test_token->value);
+            Token *test_token = generate_number(current, &current_index);
+            current_index--;
+            printf("TEST TOKEN VALUE: %s\n", test_token->value);
         }
         else if(isalpha(current[current_index])){
-            TokenKeyword *test_keyword = generate_keyword(current, &current_index);
+            Token *test_keyword = generate_keyword(current, &current_index);
+            print_token(*test_keyword);
+            current_index--;
         }
         current_index++;
     }
